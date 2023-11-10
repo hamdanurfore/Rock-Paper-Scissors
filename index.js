c5c5d0a40fe1b3c453fa7d8bb6ae75930bf4f3ca
Hamda <nurfore14@gmail.com>
2023-11-10 23:05:51 +0000

    Computer Choices

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+var computerChoices = ["r", "p", "s"];
+
 // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
+
+ // Randomly chooses a choice from the options array. This is the Computer's guess.
+
     // Collect the user's response and convert to lower case.
     // Only run game logic if user chose a valid option
         // Win/lose conditions:
+
 // When the game is over, alert the totals to the user. We can use the \n character to make a line break.
