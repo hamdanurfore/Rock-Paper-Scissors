75f05fb7d75eaac2acceb2c53df3a9e40c86e747
Hamda <nurfore14@gmail.com>
2023-11-10 23:10:11 +0000

    Number of wins, losses and ties

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 var computerChoices = ["r", "p", "s"];
 
 // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-
+var wins = 0;
+var losses = 0;
+var ties = 0;
+for (var i = 0; i < 10; i++) {
  // Randomly chooses a choice from the options array. This is the Computer's guess.
 
     // Collect the user's response and convert to lower case.
+
     // Only run game logic if user chose a valid option
         // Win/lose conditions:
+}
 
 // When the game is over, alert the totals to the user. We can use the \n character to make a line break.
